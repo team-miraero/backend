27407fb34b71c38475eb49f90c82db6d3ef82446
2021037156 <tmj5574@hanyang.ac.kr>
2026-07-24 14:15:54 +0900

    feat: config 설정 추가

--- src/main/java/org/jejuro/miraero/global/config/DataSourceConfig.java
+++ src/main/java/org/jejuro/miraero/global/config/DataSourceConfig.java
@@ -1,5 +1,38 @@
 package org.jejuro.miraero.global.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@PropertySource("classpath:application.properties")
 public class DataSourceConfig {
 
+  private final Environment environment;
+
+  public DataSourceConfig(Environment environment) {
+    this.environment = environment;
+  }
+
+  @Bean(destroyMethod = "close")
+  public HikariDataSource dataSource() {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(environment.getRequiredProperty("spring.datasource.url"));
+    config.setUsername(environment.getRequiredProperty("spring.datasource.username"));
+    config.setPassword(environment.getRequiredProperty("spring.datasource.password"));
+    config.setDriverClassName(
+        environment.getRequiredProperty("spring.datasource.driver-class-name"));
+    return new HikariDataSource(config);
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
 }
--- src/main/java/org/jejuro/miraero/global/config/MyBatisConfig.java
+++ src/main/java/org/jejuro/miraero/global/config/MyBatisConfig.java
@@ -1,5 +1,26 @@
 package org.jejuro.miraero.global.config;
 
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = "org.jejuro.miraero.domain")
 public class MyBatisConfig {
 
+  @Bean
+  public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+    SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+    factoryBean.setDataSource(dataSource);
+    factoryBean.setMapperLocations(
+        new PathMatchingResourcePatternResolver().getResources("classpath*:mappers/**/*.xml"));
+    factoryBean.setTypeAliasesPackage("org.jejuro.miraero.domain");
+    return factoryBean.getObject();
+  }
 }
