8f5cbabe37ced67cb94a637a9e017e2e788aa040
2021037156 <tmj5574@hanyang.ac.kr>
2026-07-28 22:03:49 +0900

    feat: 회원가입 DTO 추가

---
+++ src/main/java/org/jejuro/miraero/domain/user/dto/request/UserSignUpRequest.java
@@ -0,0 +1,20 @@
+package org.jejuro.miraero.domain.user.dto.request;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserSignUpRequest {
+
+  @NotBlank(message = "이메일은 필수입니다")
+  @Email(message = "이메일 형식이 올바르지 않습니다.")
+  private String email;
+
+  @NotBlank(message = "비밀번호는 필수입니다.")
+  private String password;
+
+}
---
+++ src/main/java/org/jejuro/miraero/domain/user/dto/response/UserSignUpResponse.java
@@ -0,0 +1,13 @@
+package org.jejuro.miraero.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserSignUpResponse {
+
+  private Long userId;
+  private String email;
+
+}
