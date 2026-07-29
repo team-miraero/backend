6af470b679ff25438e7642d9a737e5a3f2d12bfd
2021037156 <tmj5574@hanyang.ac.kr>
2026-07-29 16:13:28 +0900

    feat: 회원가입 비밀번호 8자 이상 제한

--- src/main/java/org/jejuro/miraero/domain/user/dto/request/UserSignUpRequest.java
+++ src/main/java/org/jejuro/miraero/domain/user/dto/request/UserSignUpRequest.java
@@ -2,6 +2,7 @@ package org.jejuro.miraero.domain.user.dto.request;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,9 @@ public class UserSignUpRequest {
   @Email(message = "이메일 형식이 올바르지 않습니다.")
   private String email;
 
+
   @NotBlank(message = "비밀번호는 필수입니다.")
+  @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
   private String password;
 
 }
