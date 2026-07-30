c931a3ac3e7a71bb4a1bbed6f4d3d2f5fbd7c7b3
2021037156 <tmj5574@hanyang.ac.kr>
2026-07-30 21:24:23 +0900

    config: Redis 연결 설정 추가

---
+++ src/main/java/org/jejuro/miraero/global/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package org.jejuro.miraero.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${redis.host}")
+  private String redisHost;
+
+  @Value("${redis.port}")
+  private int redisPort;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory() {
+    return new LettuceConnectionFactory(redisHost, redisPort);
+  }
+
+  @Bean
+  public StringRedisTemplate stringRedisTemplate(
+      RedisConnectionFactory redisConnectionFactory
+  ) {
+    return new StringRedisTemplate(redisConnectionFactory);
+  }
+}
