12e4d0a85c1c97495dba76864246a249e387483e
2021037156 <tmj5574@hanyang.ac.kr>
2026-08-04 16:36:27 +0900

    feat: 프로필 조회 서비스와 API 연결

--- src/main/java/org/jejuro/miraero/domain/user/controller/UserController.java
+++ src/main/java/org/jejuro/miraero/domain/user/controller/UserController.java
@@ -1,9 +1,29 @@
 package org.jejuro.miraero.domain.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.jejuro.miraero.domain.user.dto.response.ProfileResponse;
+import org.jejuro.miraero.domain.user.service.UserService;
+import org.jejuro.miraero.global.response.ApiResponse;
+import org.jejuro.miraero.global.security.AuthenticatedUser;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+  private final UserService userService;
+
+  @GetMapping("/profile")
+  public ResponseEntity<ApiResponse<ProfileResponse>> getProfile(
+      @AuthenticationPrincipal AuthenticatedUser user
+  ) {
+    ProfileResponse response = userService.getProfile(user.getUserId());
+
+    return ResponseEntity.ok(ApiResponse.success(response));
+  }
 }
--- src/main/java/org/jejuro/miraero/domain/user/service/UserService.java
+++ src/main/java/org/jejuro/miraero/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package org.jejuro.miraero.domain.user.service;
 
 import org.jejuro.miraero.domain.user.domain.User;
+import org.jejuro.miraero.domain.user.dto.response.ProfileResponse;
 
 public interface UserService {
 
-    User create(UserCreateCommand command);
+  User create(UserCreateCommand command);
+
+  ProfileResponse getProfile(Long userId);
 }
