c7cf23c064a4ba38d2e553016b40eac10d2bb476
2021037156 <tmj5574@hanyang.ac.kr>
2026-08-04 14:07:10 +0900

    feat: 페이스메이커 상한액 변경 DTO 추가

---
+++ src/main/java/org/jejuro/miraero/domain/pacemaker/dto/request/PaceMakerMaxAmountUpdateRequest.java
@@ -0,0 +1,11 @@
+package org.jejuro.miraero.domain.pacemaker.dto.request;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class PaceMakerMaxAmountUpdateRequest {
+
+  @NotNull(message = "자동저축 월 상한액은 필수입니다.")
+  @Positive(message = "자동저축 월 상한액은 0보다 커야 합니다.")
+  private Long maxAmount;
+}
