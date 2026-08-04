450502fc611fccf39536c163377bb74e65239c2f
itleo29 <itlei2966@gmail.com>
2026-08-04 14:26:32 +0900

    fix: Response의 YearMonth타입에 JsonFormat 지정

--- src/main/java/org/jejuro/miraero/domain/goal/dto/response/GoalPeriodResponse.java
+++ src/main/java/org/jejuro/miraero/domain/goal/dto/response/GoalPeriodResponse.java
@@ -1,6 +1,7 @@
 package org.jejuro.miraero.domain.goal.dto.response;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +14,13 @@ import java.time.YearMonth;
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class GoalPeriodResponse {
     private Integer goalMonths;
+
+    @JsonFormat(pattern = "yyyy-MM")
     private YearMonth startDate;
+
+    @JsonFormat(pattern = "yyyy-MM")
     private YearMonth endDate;
     private Integer remainMonths;
 }
