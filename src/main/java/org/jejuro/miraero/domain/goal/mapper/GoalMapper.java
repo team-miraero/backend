1040b2c9c621b97b35dd868f7a06d12f25c700d7
2021037156 <tmj5574@hanyang.ac.kr>
2026-08-11 16:21:19 +0900

    style: GoalMapper 코드 형식 정리

--- src/main/java/org/jejuro/miraero/domain/goal/mapper/GoalMapper.java
+++ src/main/java/org/jejuro/miraero/domain/goal/mapper/GoalMapper.java
@@ -1,40 +1,47 @@
 package org.jejuro.miraero.domain.goal.mapper;
 
 
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.jejuro.miraero.domain.goal.domain.Goal;
 import org.jejuro.miraero.domain.goal.domain.GoalStatus;
 
-import java.util.List;
-
 @Mapper
 public interface GoalMapper {
-    void save(Goal goal);
-    List<Goal> findGoalsByUserId(@Param("userId") Long userId);
-    Goal findById(@Param("goalId") Long goalId);
-    Goal findByIdAndUserId(
-            @Param("userId") Long userId,
-            @Param("goalId") Long goalId
-    );
-    int update(@Param("goal") Goal goal);
-    void delete(@Param("goalId") Long goalId);
-    void updateCollection(
-            @Param("userId") Long userId,
-            @Param("goalId") Long goalId
-    );
 
-    void updateCompleteStatus(
-            @Param("goalId") Long goalId
-    );
-    List<Goal> findGoalCollectionsByUserId(
-            @Param("userId") Long userId
-    );
+  void save(Goal goal);
 
-    void updateStatus(
-            Long goalId,
-            GoalStatus status
-    );
+  List<Goal> findGoalsByUserId(@Param("userId") Long userId);
 
-    boolean existsActiveGoalByUserId(@Param("userId") Long userId);
+  Goal findById(@Param("goalId") Long goalId);
+
+  Goal findByIdAndUserId(
+      @Param("userId") Long userId,
+      @Param("goalId") Long goalId
+  );
+
+  int update(@Param("goal") Goal goal);
+
+  void delete(@Param("goalId") Long goalId);
+
+  void updateCollection(
+      @Param("userId") Long userId,
+      @Param("goalId") Long goalId
+  );
+
+  void updateCompleteStatus(
+      @Param("goalId") Long goalId
+  );
+
+  List<Goal> findGoalCollectionsByUserId(
+      @Param("userId") Long userId
+  );
+
+  void updateStatus(
+      Long goalId,
+      GoalStatus status
+  );
+
+  boolean existsActiveGoalByUserId(@Param("userId") Long userId);
 }
