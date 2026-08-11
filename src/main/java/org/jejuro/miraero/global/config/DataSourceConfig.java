8df95d37619eacc36cc72576c229138f30392820
2021037156 <tmj5574@hanyang.ac.kr>
2026-08-11 14:51:47 +0900

    chore: 실행 환경별 설정 파일 분리

--- src/main/java/org/jejuro/miraero/global/config/DataSourceConfig.java
+++ src/main/java/org/jejuro/miraero/global/config/DataSourceConfig.java
@@ -6,12 +6,17 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySources({
+    @PropertySource("classpath:application.properties"),
+    @PropertySource("classpath:application-${spring.profiles.active:local}.properties")
+})
+
 public class DataSourceConfig {
 
   private final Environment environment;
