0a70ff077872dc8a1ef7e2e5a0bf7c0025e07d7d
itleo29 <itlei2966@gmail.com>
2026-08-12 14:21:57 +0900

    fix:autotransfer 생성자 기반 매핑 오류 수정

--- src/main/java/org/jejuro/miraero/domain/autotransfer/domain/AutoTransfer.java
+++ src/main/java/org/jejuro/miraero/domain/autotransfer/domain/AutoTransfer.java
@@ -1,13 +1,17 @@
 package org.jejuro.miraero.domain.autotransfer.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AutoTransfer {
     private Long autoTransferId;
     private Long withdrawalAccountId;
